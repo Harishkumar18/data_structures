8e425e9e68de807beca52cc973404a1333c42e80
Harish <HarishKumar.S@netapp.com>
2020-05-23 13:07:38 +0530

    linked list and its reversal added

---
+++ linked_list/linked_list_implementation.py
@@ -0,0 +1,47 @@
+# single node of singly linked list
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # insertion method for linked list
+    def insert(self, data):
+        newNode = Node(data)
+        if self.head:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = newNode
+        else:
+            self.head = newNode
+
+    # print elements of the linked list
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data)
+            current = current.next
+
+
+# ll = LinkedList()
+# ll.insert(1)
+# ll.insert(2)
+# ll.insert(3)
+# ll.insert(4)
+# ll.insert(5)
+# ll.print_list()
+# creating first node
+# n1 = Node(3)
+# print(n1.data)
+# print(n1.next)
+
+# creating linked list
+# ll = LinkedList()
+# ll.head = Node(3)
+# print(ll.head.data)
+# print(ll.head.next)
---
+++ linked_list/reverse_linked_list.py
@@ -0,0 +1,33 @@
+"""
+https://www.pythoncentral.io/reverse-singly-linked-list/
+"""
+from linked_list.linked_list_implementation import LinkedList
+
+
+def reverseList(linked_list):
+    """Iterative solution"""
+    prev = None
+    current = linked_list.head
+    next_ele = current.next
+    while current:
+        current.next = prev
+        prev = current
+        current = next_ele
+        if next_ele:
+            next_ele = next_ele.next
+    ll.head = prev
+
+
+
+ll = LinkedList()
+ll.insert(1)
+ll.insert(2)
+ll.insert(3)
+ll.insert(4)
+ll.insert(5)
+print(f"Linked List")
+print(ll.print_list())
+print(f"reversed linked list")
+reverseList(ll)
+ll.print_list()
+
